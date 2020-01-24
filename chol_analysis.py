1d78f24de725d5f060cf04fda5dfc3e834bc56de
Jiacheng Lin <jl915@duke.edu>
2020-01-24 12:35:04 -0500

    Added option 1 for cholesterol analysis input

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,12 +1,24 @@
+
+
+def cholesterol_analysis():
+    print("cholesterol_analysis")
+    HDLinput = input("Enter test result: ")
+    test_info = HDLinput.split("=")
+    if test_info[0] == "HDL":
+        HDL_analysis(test_info[1])
+
 def interface():
     choice = 0
     while choice != "9":
         print("Cholesterol Calculator")
         print("Options: ")
+        print(" 1 - Cholesterol Analysis")
         print(" 9 - Quit")
         choice = input("Enter your option: ")
         if choice == '9':
             return
+        elif choice == "1":
+            cholesterol_analysis()
     
 
 if __name__ == "__main__":
